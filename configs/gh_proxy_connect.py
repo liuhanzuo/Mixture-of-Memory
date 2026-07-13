4ac78a2fad551131e3ead60de44993aa83813b7d
LiuHanzuo <lhz24@mails.tsinghua.edu.cn>
2026-07-13 12:45:45 +0800

    chore(infra): github SSH-over-proxy push setup (deploy key gitignored + CONNECT tunnel helper)

---
+++ configs/gh_proxy_connect.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""SSH ProxyCommand: tunnel to <host> <port> through the star HTTP proxy via CONNECT.
+Usage (as ssh ProxyCommand): gh_proxy_connect.py %h %p
+"""
+import sys, socket, select, os
+PROXY = ("star-proxy.oa.com", 3128)
+th, tp = sys.argv[1], int(sys.argv[2])
+s = socket.create_connection(PROXY, timeout=15)
+s.sendall(("CONNECT %s:%d HTTP/1.0\r\nHost: %s:%d\r\n\r\n" % (th, tp, th, tp)).encode())
+buf = b""
+while b"\r\n\r\n" not in buf:
+    ch = s.recv(1)
+    if not ch:
+        sys.stderr.write("proxy closed during CONNECT\n"); sys.exit(1)
+    buf += ch
+if b" 200 " not in buf.split(b"\r\n", 1)[0]:
+    sys.stderr.write("proxy CONNECT failed: " + buf.split(b"\r\n")[0].decode("latin1") + "\n"); sys.exit(1)
+s.setblocking(False)
+fin, fout = sys.stdin.fileno(), sys.stdout.fileno()
+while True:
+    r, _, _ = select.select([fin, s], [], [])
+    if fin in r:
+        d = os.read(fin, 65536)
+        if not d: break
+        s.sendall(d)
+    if s in r:
+        try:
+            d = s.recv(65536)
+        except (BlockingIOError, InterruptedError):
+            continue
+        if not d: break
+        os.write(fout, d)
